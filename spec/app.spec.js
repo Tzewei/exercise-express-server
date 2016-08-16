e14f34b4f6d9670b914798d5865a841636d29c95
chongtw0990@gmail.com <chongtw0990@gmail.com>
2016-08-16 22:07:37 +0800

    Add test spec

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -22,44 +22,77 @@ describe("Express Server API", function() {
 })
 
 describe("Express Server API", function() {
-  describe("should return users json on get /users", function() {
+  describe("should return profiles json on get /profiles", function() {
       it("returns status code 200", function(done) {
         supertest(app)
-          .get('/users')
+          .get('/profiles')
           .expect('Content-Type', /json/)
           .expect(200, done);
       });
 
+      it("returns status code 404", function(done) {
+        supertest(app)
+          .get('/profiles/edit')
+          .expect('Content-Type', /json/)
+          .expect(404, done);
+      });
+
       it("returns a correct json format", function(done) {
         supertest(app)
-          .get('/users')
-          .expect([
+          .get('/profiles')
+          .expect(
             {
-              name: 'Gerald Ang',
-              age: 33,
-              email: 'geraldangw@gmail.com'
-            },
-            {
-              name: 'benyang',
-              age: 31,
-              email: 'benyang00@gmail.com'
-            },
-            {
-              name: 'weiyang',
-              age: 81,
-              email: 'thetaiwanese@gmail.com'
-            },
-            {
-              name: 'jen',
-              age: 12,
-              email: 'jen@gmail.com'
-            },
-            {
-              name: 'chong',
-              age: 25,
-              email: 'chong@gmail.com'
-            },
-          ], done);
+              name: 'Chong Tze Wei',
+              email: 'chongtw0990@gmail.com',
+              contactnum: '+6591190990',
+              hobbies: 'Watch youtube, cycling',
+              facebook: 'https://www.facebook.com/tzewei.chong.96',
+              emailme: 'mailto:chongtw0990@gmail.com'
+            }, done);
       });
   });
 });
+
+
+// describe("Express Server API", function() {
+//   describe("should return users json on get /users", function() {
+//       it("returns status code 200", function(done) {
+//         supertest(app)
+//           .get('/users')
+//           .expect('Content-Type', /json/)
+//           .expect(200, done);
+//       });
+//
+//       it("returns a correct json format", function(done) {
+//         supertest(app)
+//           .get('/users')
+//           .expect([
+//             {
+//               name: 'Gerald Ang',
+//               age: 33,
+//               email: 'geraldangw@gmail.com'
+//             },
+//             {
+//               name: 'benyang',
+//               age: 31,
+//               email: 'benyang00@gmail.com'
+//             },
+//             {
+//               name: 'weiyang',
+//               age: 81,
+//               email: 'thetaiwanese@gmail.com'
+//             },
+//             {
+//               name: 'jen',
+//               age: 12,
+//               email: 'jen@gmail.com'
+//             },
+//             {
+//               name: 'chong',
+//               age: 25,
+//               email: 'chong@gmail.com'
+//             },
+//           ], done);
+//       });
+//   });
+// });
